174e7a00d3e96e398b7e14919e8b2de9f70e9f66
Mubashir Shah <g9syedmubashirshah@gmail.com>
2024-06-07 11:25:03 +0500

    first Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let gender = "female";
+// let age = 6;
+
+
+// if(gender == "female"){
+// console.log("50% discount on ticket");
+
+// }else if (age <= 5 ){
+//     console.log("give him/her free on ticket");
+// }else if( age > 8){
+//     console.log("haf price on ticket");
+// }else if( age > 65){
+//     console.log("30% discount on ticket");
+// }else{
+//     console.log("normal price on ticket");
+// }
